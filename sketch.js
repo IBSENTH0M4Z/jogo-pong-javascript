b913c9a0ab852e1652d2464c213406fdab458e8f
IBSENTH0M4Z <106695831+IBSENTH0M4Z@users.noreply.github.com>
2022-06-15 14:35:24 -0300

    adicionando o arquivo do jogo

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let xbolinha = 300;
+let ybolinha = 200;
+let diametro = 22;
+
+let velocidadexbolinha = 6;
+let velocidadeybolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+circle(xbolinha,ybolinha,diametro)
+  xbolinha += velocidadexbolinha;
+  //ybolinha += velocidadeybolinha;
+  
+//if (xbolinha  width){
+  
+  
+
+}
